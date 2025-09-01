c6288f46ccc9e629d2688bc4255f68fc9e7a6967
David Zamora Balmes <davidzamora@MacBook-Pro-de-David.local>
2025-09-01 18:08:12 +0200

    test: afegeix ci_gate_probe per garantir cobertura real a lib/utils/

---
+++ test/utils/ci_gate_probe_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kashly_app/utils/ci_gate_probe.dart';
+
+void main() {
+  test('inc increments', () {
+    expect(inc(1), 2);
+  });
+}
